24f4081669c2f007fadab538c0f98105ddbca897
ahmed3salah <31387314+ahmed3salah@users.noreply.github.com>
2025-02-06 21:57:42 +0000

    Fix styling

--- src/routes/api.php
+++ src/routes/api.php
@@ -30,7 +30,7 @@ Route::get('api/v1/kenda-communication', function (Request $request) {
     $functionName = $request->get('function');
     $parameters = $request->get('parameters');
 
-    if(!array_key_exists($functionName, config('kenda-communication-plugin.functions'))) {
+    if (! array_key_exists($functionName, config('kenda-communication-plugin.functions'))) {
         return response()->json([
             'message' => 'Function not found',
         ], 404);
@@ -46,7 +46,7 @@ Route::get('api/v1/kenda-communication', function (Request $request) {
         ], 404);
     }
 
-    if(!$functionClass->isSubclassOf('Kenda\KendaCommunicationPlugin\Functions\KendaFunction')) {
+    if (! $functionClass->isSubclassOf('Kenda\KendaCommunicationPlugin\Functions\KendaFunction')) {
         return response()->json([
             'message' => 'Function not found',
         ], 404);
