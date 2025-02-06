b71157793c8ccd9463ee25b03294fd721da55811
Ahmed Salah <31387314+ahmed3salah@users.noreply.github.com>
2025-02-06 22:53:38 +0200

    Add user resolving section to configuration

--- config/kenda-communication-plugin.php
+++ config/kenda-communication-plugin.php
@@ -2,6 +2,11 @@
 
 // config for Kenda/KendaCommunicationPlugin
 return [
+
+    /* -------------------------------------------------------------------------- */
+    /*                                User Resolving                              */
+    /* -------------------------------------------------------------------------- */
+
     'user_model' => 'App\Models\User',
     'user_phone_number_column' => 'phone_number',
 ];
