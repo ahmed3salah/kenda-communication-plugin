ea9e46eeb7f70c60add9b56f7f5ffbcdacacf3e5
Ahmed Salah <31387314+ahmed3salah@users.noreply.github.com>
2025-02-07 14:25:42 +0200

    feat(api): update endpoint to use POST method and controller action
    refactor the `kenda-communication` endpoint to use POST instead of GET.
    This change directs requests to the `KendaCommunicationProtocolController`
    for better handling of communication protocols.

--- routes/api.php
+++ routes/api.php
@@ -1,61 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('api/v1/kenda-communication', function (Request $request) {
-    $request->validate([
-        'senderPhoneNumber' => ['required', 'string'],
-        'function' => ['required', 'string'],
-        'parameters' => ['nullable', 'array'],
-    ]);
-
-    // Try to get the user of the request
-
-    if (config('kenda-communication-plugin.enable_user_resolving')) {
-        $userModel = config('kenda-communication-plugin.user_model');
-        $userPhoneNumberColumn = config('kenda-communication-plugin.user_phone_number_column');
-
-        $resolvedUser = $userModel::where($userPhoneNumberColumn, $request->senderPhoneNumber)->first();
-
-        ray($resolvedUser);
-
-        // if(!$resolvedUser) {
-        //     return response()->json([
-        //         'message' => 'User not found',
-        //     ], 404);
-        // }
-    }
-
-    $functionName = $request->get('function');
-    $parameters = $request->get('parameters');
-
-    if (! array_key_exists($functionName, config('kenda-communication-plugin.functions'))) {
-        return response()->json([
-            'message' => 'Function not found',
-        ], 404);
-    }
-
-    $functionClass = config('kenda-communication-plugin.functions')[$functionName];
-
-    try {
-        $functionClass = new ReflectionClass($functionClass);
-    } catch (ReflectionException $e) {
-        return response()->json([
-            'message' => 'Function not found',
-        ], 404);
-    }
-
-    if (! $functionClass->isSubclassOf('Kenda\KendaCommunicationPlugin\Functions\KendaFunction')) {
-        return response()->json([
-            'message' => 'Function not found',
-        ], 404);
-    }
-
-    $result = (new $functionClass($parameters, $userModel))->execute();
-
-    return response()->json([
-        'message' => 'Function executed successfully',
-        'result' => $result,
-    ]);
-});
+Route::post('api/v1/kenda-communication',
+    [\Kenda\KendaCommunicationPlugin\Controllers\KendaCommunicationProtocolController::class, 'process']);
---
+++ src/Controllers/KendaCommunicationProtocolController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Kenda\KendaCommunicationPlugin\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class KendaCommunicationProtocolController
+{
+    public function process(Request $request)
+    {
+
+        $request->validate([
+            'senderPhoneNumber' => ['required', 'string'],
+            'function' => ['required', 'string'],
+            'parameters' => ['nullable', 'array'],
+        ]);
+
+        // Resolve the user
+        $userModel = null;
+        if (config('kenda-communication-plugin.enable_user_resolving')) {
+            $userPhoneNumberColumn = config('kenda-communication-plugin.user_phone_number_column');
+            // user model class (App\Models\User)
+            $userModelClass = config('kenda-communication-plugin.user_model');
+            $userModel = $userModelClass::where($userPhoneNumberColumn, $request->senderPhoneNumber)->first();
+        }
+
+        if (is_null($userModel) && !config('kenda-communication-plugin.enable_guest_user')) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        $functionName = $request->get('function');
+        $parameters = $request->get('parameters');
+
+        if (!array_key_exists($functionName, config('kenda-communication-plugin.functions'))) {
+            return response()->json([
+                'message' => 'Function not found',
+            ], 404);
+        }
+
+        $functionClass = config('kenda-communication-plugin.functions')[$functionName];
+
+        try {
+            $functionClass = new ReflectionClass($functionClass);
+        } catch (ReflectionException $e) {
+            return response()->json([
+                'message' => 'Function not found',
+            ], 404);
+        }
+
+        if (!$functionClass->isSubclassOf('Kenda\KendaCommunicationPlugin\Functions\KendaFunction')) {
+            return response()->json([
+                'message' => 'Function not found',
+            ], 404);
+        }
+
+        $result = $functionClass->newInstance($parameters, $userModel)->execute();
+
+        dd($result);
+
+
+        return response()->json([
+            'message' => 'Function executed successfully',
+            'result' => $result,
+        ]);
+    }
+}
